2a4b3e8c791300d861a138ec8cef128ebcf70329
rylabs-billy <bthompso@akamai.com>
2024-12-31 20:33:33 -0500

    echo hosts

--- scripts/run.sh
+++ scripts/run.sh
@@ -70,6 +70,7 @@ function test {
   build
   echo "[info] running ansible playbooks in check mode"
   ansible-playbook -v -i hosts provision.yml --check --extra-vars "@info.yml"
+  echo hosts
   ansible-playbook -v -i hosts site.yml --check
 }
 
