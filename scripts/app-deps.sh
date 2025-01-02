975dc71f4c025a9126990a538c82b301f8aa70c4
rylabs-billy <bthompso@akamai.com>
2025-01-02 15:06:04 -0500

    update app-deps.sh

--- scripts/app-deps.sh
+++ scripts/app-deps.sh
@@ -4,17 +4,23 @@
 # configures test environment for ansible check mode
 
 function playbook_deps {
+  echo "[info] Installing playbook dependencies"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install yq
   # required files from apt install
   apt install fail2ban -y
 }
 
 function kafka_deps {
+  echo "[info] Configure Kafka dependencies"
+  defaults="roles/kafka/defaults/main.yml"
+
   # add kafka user and group
-  useradd --system -s /usr/bin/nologin -m kafka
+  useradd --system -s /usr/bin/nologin -m $(cat $defaults | yq .kafka_data_directory)
 
   # write dirs and vars from roles/kafka/defaults/main.yml
   kafka_file_list=($(
-    cat roles/kafka/defaults/main.yml \
+    cat $defaults \
     | grep -Ev '#|---' \
     | IFS='\n' awk '{print $2}' \
     | tr '\n' ' '
